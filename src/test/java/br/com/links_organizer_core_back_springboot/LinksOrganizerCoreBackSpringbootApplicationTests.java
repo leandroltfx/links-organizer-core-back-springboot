827e96351bea5892d69fd5cc4613c42c4ad15caa
leandroltfx <leandroltfbr@hotmail.com>
2025-09-07 08:57:14 -0300

    initial commit

---
+++ src/test/java/br/com/links_organizer_core_back_springboot/LinksOrganizerCoreBackSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.links_organizer_core_back_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinksOrganizerCoreBackSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
