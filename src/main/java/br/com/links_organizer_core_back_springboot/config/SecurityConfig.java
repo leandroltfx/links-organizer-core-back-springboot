29e39c0c0521ab28cceb6ae69bacec05d8a037a8
leandroltfx <leandroltfbr@hotmail.com>
2025-09-16 08:34:34 -0300

    adiciona dependência do spring security e desabilita sua configuração padrão para que se possa customizá-la

---
+++ src/main/java/br/com/links_organizer_core_back_springboot/config/SecurityConfig.java
@@ -0,0 +1,18 @@
+package br.com.links_organizer_core_back_springboot.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        // Desabilitando as configurações padrão do Spring Security
+        httpSecurity.csrf(csrf -> csrf.disable());
+        return httpSecurity.build();
+    }
+
+}
