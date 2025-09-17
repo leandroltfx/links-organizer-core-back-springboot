784b92ea30fc49863c9cc3d2e90d121f14d9d8a1
leandroltfx <leandroltfbr@hotmail.com>
2025-09-17 08:43:36 -0300

    configura criptografia de senha no cadastro de usuário

--- src/main/java/br/com/links_organizer_core_back_springboot/modules/user/useCases/CreateUserUseCase.java
+++ src/main/java/br/com/links_organizer_core_back_springboot/modules/user/useCases/CreateUserUseCase.java
@@ -5,6 +5,7 @@ import br.com.links_organizer_core_back_springboot.modules.user.entities.UserEnt
 import br.com.links_organizer_core_back_springboot.modules.user.model.dto.UserRegistrationResponseDto;
 import br.com.links_organizer_core_back_springboot.modules.user.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,6 +14,9 @@ public class CreateUserUseCase {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public UserRegistrationResponseDto execute(UserEntity userEntity) {
         this.userRepository.findByUserNameOrEmail(
                 userEntity.getUserName(),
@@ -20,6 +24,8 @@ public class CreateUserUseCase {
         ).ifPresent(user -> {
             throw new UserFoundException("Este e-mail e/ou nome de usuário já está em uso.");
         });
+
+        userEntity.setPassword(passwordEncoder.encode(userEntity.getPassword()));
 
         this.userRepository.save(userEntity);
         return UserRegistrationResponseDto
