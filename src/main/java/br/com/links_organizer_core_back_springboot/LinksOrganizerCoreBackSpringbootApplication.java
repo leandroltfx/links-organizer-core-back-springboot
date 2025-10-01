7a325fdf0e94c012cf6933736d5feff46626d85d
leandroltfx <leandroltfbr@hotmail.com>
2025-10-01 08:17:17 -0300

    initial commit

---
+++ src/main/java/br/com/links_organizer_core_back_springboot/LinksOrganizerCoreBackSpringbootApplication.java
@@ -0,0 +1,13 @@
+package br.com.links_organizer_core_back_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinksOrganizerCoreBackSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinksOrganizerCoreBackSpringbootApplication.class, args);
+	}
+
+}
