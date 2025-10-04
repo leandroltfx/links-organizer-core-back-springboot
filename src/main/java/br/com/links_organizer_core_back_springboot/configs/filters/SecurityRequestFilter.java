afe08a2df44fccea12b186a9a96d80b778e738af
leandroltfx <leandroltfbr@hotmail.com>
2025-10-04 10:41:03 -0300

    corrige formato de resposta em caso de token inválido

--- src/main/java/br/com/links_organizer_core_back_springboot/configs/filters/SecurityRequestFilter.java
+++ src/main/java/br/com/links_organizer_core_back_springboot/configs/filters/SecurityRequestFilter.java
@@ -1,11 +1,15 @@
 package br.com.links_organizer_core_back_springboot.configs.filters;
 
+import br.com.links_organizer_core_back_springboot.dtos.ErrorDTO;
 import br.com.links_organizer_core_back_springboot.providers.JWTProvider;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -30,7 +34,17 @@ public class SecurityRequestFilter extends OncePerRequestFilter {
             var token = this.jwtProvider.validateToken(header);
 
             if (token == null) {
-                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                ErrorDTO errorDTO = new ErrorDTO("Token inválido");
+
+                // Converte para JSON
+                ObjectMapper mapper = new ObjectMapper();
+                String body = mapper.writeValueAsString(errorDTO);
+
+                // Define resposta
+                response.setStatus(HttpStatus.FORBIDDEN.value());
+                response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+                response.getWriter().write(body);
+                response.getWriter().flush();
                 return;
             }
 
