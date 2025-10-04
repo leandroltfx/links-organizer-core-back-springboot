1de9f8d343959ff3e6e448a9ccc425a018a77751
leandroltfx <leandroltfbr@hotmail.com>
2025-10-04 09:34:44 -0300

    configura título e descrição no swagger

--- src/main/java/br/com/links_organizer_core_back_springboot/LinksOrganizerCoreBackSpringbootApplication.java
+++ src/main/java/br/com/links_organizer_core_back_springboot/LinksOrganizerCoreBackSpringbootApplication.java
@@ -1,9 +1,12 @@
 package br.com.links_organizer_core_back_springboot;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Links Organizer", description = "API core do Links Organizer", version = "1"))
 public class LinksOrganizerCoreBackSpringbootApplication {
 
 	public static void main(String[] args) {
